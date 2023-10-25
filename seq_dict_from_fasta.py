deda3e9b218fb1eeb4f1a11191838fb38dc4ad55
pfb12 <pfb12@info12.cshl.edu>
2023-10-25 12:01:21 -0400

    making fasta parser global

--- seq_dict_from_fasta.py
+++ seq_dict_from_fasta.py
@@ -10,39 +10,43 @@ import re
 	# the seq_id, description, and sequence from the fasta 
 	# records into a nested dictionary.
 
-def seq_dict_from_fasta(input_file):
-	input_file = sys.argv[1]
-	
-	usage = f'\n\n\tusage: {sys.argv[0]} filename.fasta'
-	
-	if len(sys.argv) < 2:
-		sys.stderr.write(usage)
-		sys.exit(1)
-
-	fastaDict = {}
-	with open(input_file, "r") as fasta_all:
+def seq_dict_from_fasta_file(input_file):
+    fastaDict = {}
+    with open(input_file, "r") as fasta_all:
 		
 		# remove end of line characters
-		for line in fasta_all:
-			line = line.rstrip()
+        for line in fasta_all:
+            line = line.rstrip()
 			
 			# find headers
-			if line.startswith('>'):
+            if line.startswith('>'):
 				
 				# capture header information
-				match = re.search(r'^>(\S+)\s+(.+)$', line)
-				seq_id = match.group(1)
-				desc = match.group(2)
+                match = re.search(r'^>(\S+)\s+(.+)$', line)
+                seq_id = match.group(1)
+                desc = match.group(2)
 		
 				# add header information to dictionary
-				fastaDict[seq_id] = {'desc':desc,
+                fastaDict[seq_id] = {'desc':desc,
 													 'seq':""}
 
 			# find sequences and add them to dictionary
-			else:
-				fastaDict[seq_id]['seq'] += line
+            else:
+                fastaDict[seq_id]['seq'] += line
 		
-	print(fastaDict)
+    print(fastaDict)
 
-	if __name__ = '__main__':
-		main()
+
+def main():
+    input_file = sys.argv[1]
+
+    usage = f'\n\n\tusage: {sys.argv[0]} filename.fasta'
+
+    if len(sys.argv) <2:
+        sys.stderr.write(usage)
+        sys.exit(1)
+
+    seq_dict_from_fasta_file(input_file)
+
+if __name__ == '__main__':
+    main()
