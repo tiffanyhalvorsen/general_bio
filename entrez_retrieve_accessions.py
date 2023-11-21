b649e570f1b0d5514e9a862840b99ee915520d06
tiffanyhalvorsen <tiffany.marie.halvorsen@gmail.com>
2023-11-20 17:48:03 -0800

    accession retrieval with output file works for genome and protein databases

--- entrez_retrieve_accessions.py
+++ entrez_retrieve_accessions.py
@@ -6,57 +6,60 @@ import subprocess
 import sys
 
 
-def entrez_retrieve_accessions(db, term, return_max):
+def entrez_retrieve_accessions(db, term, output_file, retmax):
 
 	## retrieves records from Entrez database
 
 	Entrez.email = "halvorsen1@llnl.gov"
-	handle = Entrez.esearch(db=db, term=term, retmax=return_max)
+	handle = Entrez.esearch(idtype="acc", db=db, term=term, retmax=retmax)
 	record = Entrez.read(handle)
 	
 	## esearch returns uid numbers (unique identifiers). Use the uid to retrieve
 	## accession numbers for each genome with efetch, or from a different database
 	## with elink
-
+	
 	uids = record['IdList']
-	print(uids)
+	total_records = len(uids)
 	if db == "genome":
 		
 		# obtain nucleotide records from genome search
 
-		genome_handle = Entrez.elink(dbfrom="genome", db="nuccore", from_uid=uids)
+		genome_handle = Entrez.elink(dbfrom="genome", db="nucleotide", from_uid=uids)
 		genome_record = Entrez.read(genome_handle)
 
-		print('\n\n',genome_record)
-		
 		## extract nucleotide database accessions from the Entrez.elink() object
 		genome_accessions = genome_record[0]['LinkSetDb'][0]['Link']
 		ids = [acc['Id'] for acc in genome_accessions]
-		print('\n',genome_accessions,'\n\n',ids,'\n\n',len(ids))
-		total_genomes = len(ids)
+		
 		genome_handle.close()
 
 		accessions = list()
 		
 
-		## figure out how to get primary accessions with efetch for genomes
-		## i think maybe just doing esearch directly to efetch and set format equal to acc
+		## use efetch to get primary accession numbers from uids
 
 		for acc in ids:
 			# retrieve record                                          
-			acc_handle = Entrez.efetch(db='nuccore', id=acc, retmode="xml")   
+			acc_handle = Entrez.efetch(db='nucleotide', id=acc, retmode="xml")   
 			acc_record = Entrez.read(acc_handle)
+			#print(acc_record[0]['GBSeq_primary-accession'])
 			# make list of primary accessions                          
 			accessions.append(acc_record[0]['GBSeq_primary-accession'])
-			print(acc_record)
 			acc_handle.close()                                         
-                                                               
-		print(accessions)                                              
+        
+		with open(output_file, "w") as output:
+			accessions_delim = '\n'.join(accessions)
+			output.write(f'# Total number of genomes retrieved with esearch term {term}: {total_records}.\n')
+			output.write(accessions_delim)
+		 
 
-		return accessions, total_genomes
+		print(f'{total_records} total UIDs accessed from nucleotide db  and {len(accessions)} resulting accessions written to {output_file}')
+		
+		return accessions, output_file
 
 	elif db == "protein":
 		
+		## make list of uids from elink for retrieving accessions from another database
 
 		#prot_handle = Entrez.elink(dbfrom="protein", db=db, from_uid=uids)
 		#prot_record = Entrez.read(prot_handle)
@@ -64,8 +67,6 @@ def entrez_retrieve_accessions(db, term, return_max):
 		#prot_accessions = prot_handle[1]['LinkSetDb'][0]['Link']
 		#accessions = [acc['Id'] for acc in prot_accessions]
 		#total_prot = len(prot_accessions)
-
-		#print('\n',record, '\n\n', total_prot, '\n\n', accessions)
 
 		#protein_handle.close()
 
@@ -78,25 +79,27 @@ def entrez_retrieve_accessions(db, term, return_max):
 			# retrieve record
 			acc_handle = Entrez.efetch(db=db, id=uid, retmode="xml")
 			acc_record = Entrez.read(acc_handle)
-
+			
 			# make list of primary accessions
 			accessions.append(acc_record[0]['GBSeq_primary-accession'])
 			acc_handle.close()
 
-		print(accessions)
-		
 		## for now, return the accessions from Entrez.esearch() simply using the user-
 		## provided database
-
-		return accessions
+		with open(output_file, "w") as output:
+			accessions_delim = '\n'.join(accessions)
+			output.write(accessions_delim)
 		
-	handle.close()
-
-#def entrez_retrieve_seqs(acc_list):
-
+		
+		print(f'{total_records} total UIDs accessed from {db} using esearch term {term} and {len(accessions)} resulting accessions written to {output_file}')	
 	
+		return accessions, output_file
 
-accessions = list()
+	else:
+		sys.stderr.write(usage)
+		sys.exit(1)
+	
+	handle.close()
 
 #def blastx_on_accession_list(accession_file):
 #	with open(accession_file, "r") as accession_file_obj:
@@ -118,23 +121,23 @@ accessions = list()
 		
 def main():
 	
-	usage = f'\n\n\tusage: {sys.argv[0]} "database type" "search term"  "ID type"\n\n\texamples:\n\n\tdatabase type: "pubmed" or "nucleotide"\n\tsearch term: "Pseudomonas[Orgn] AND luxR[Gene]"\n\n\toptional 3rd parameter: max # of results to return (default is 20)\n\n'
+	usage = f'\n\n\tusage: {sys.argv[0]} "database type" "search term"  "output_file.txt" max # of results to return (optional, default is 20)\n\n\texamples:\n\n\tdatabase type: "genome" or "protein"\n\tsearch term: "Pseudomonas[Orgn] AND luxR[Gene]"\n\n\t'
 
 
-	if len(sys.argv) < 3:
+	if len(sys.argv) < 4:
 		sys.stderr.write(usage)
 		sys.exit(1)
 
-	if len(sys.argv) == 4:	
-		return_max = sys.argv[3]
+	if len(sys.argv) == 5:	
+		retmax = sys.argv[4]
 	else:
-		return_max = 20
+		retmax = 20
 
 	db = sys.argv[1]
 	term = sys.argv[2]
+	output_file = sys.argv[3]
 
-	entrez_retrieve_accessions(db, term, return_max)	
-
+	entrez_retrieve_accessions(db, term, output_file, retmax)
 
 if __name__ == '__main__':
 	main()
