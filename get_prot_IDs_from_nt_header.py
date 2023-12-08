d3e34cdb1143ee2c2e4104914800a35a004a4e5e
tiffanyhalvorsen <tiffany.marie.halvorsen@gmail.com>
2023-12-08 10:39:33 -0800

    script for extracting protein accessions from FASTA headers

---
+++ get_prot_IDs_from_nt_header.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+from Bio import SeqIO
+import re
+import os
+
+## This script retrieves protein IDs from the header ID line of 
+## a fasta file using regex. The fasta file can be nucleotide or protein
+## sequences.
+
+## usage: /path/to/script input.fasta
+
+def get_prot_IDs(input_fasta):
+	name, ext = os.path.splitext(input_fasta)
+	output_file = f'{name}_protIDs.txt'
+	
+	with open(output_file, "w") as output:
+		for record in SeqIO.parse(input_fasta, "fasta"):
+			match = re.search(r'.\d_cds_(.+.\d)_\d+$', record.id)
+			prot_ID = match.group(1)
+			output.write(f'{prot_ID}\n')	
+
+	return output
+
+
+def main():
+
+	input_fasta = sys.argv[1]
+	
+	get_prot_IDs(input_fasta)
+
+if __name__ == '__main__':
+	main()
+
+
