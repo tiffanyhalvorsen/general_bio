d709cf8fba3956b6bc4076b15d2cd78185cbbf2a
tiffanyhalvorsen <tiffany.marie.halvorsen@gmail.com>
2023-12-08 11:40:38 -0800

    retrieve protein sequences from Entrez using input line-separated accession list file and write headers + seqs to a combined FASTA file

---
+++ entrez_get_prots_from_acc.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+from Bio import Entrez
+from Bio import SeqIO
+from Bio import GenBank
+
+## This script retrieves protein sequences from a list of accessions.
+## Accession list should be provided as a <txt> file with line-
+## separated protein accession version numbers.
+
+## Usage: /path/to/script acc_list.txt
+
+def entrez_get_prots_from_acc(acc_file):
+	
+	name, ext = os.path.splitext(acc_file)
+	name = name.replace("_protIDs", "")
+	output_file = f'{name}.faa'
+	Entrez.email = "halvorsen1@llnl.gov"
+	
+	accs = list()
+	with open(acc_file, "r") as acc_list:
+		for line in acc_list:
+			line = line.rstrip()
+			if not line.startswith('#'):
+				accs.append(line)
+
+	accs_str = ",".join(str(acc) for acc in accs)
+
+	with open(output_file, "w") as output:
+		handle = Entrez.efetch(db='protein', id=accs_str, rettype = "fasta", retmode="text")
+		for seq_record in SeqIO.parse(handle, "fasta"):
+			output.write(f'>{seq_record.description}\n{seq_record.seq}\n')
+		handle.close()
+			
+	return output
+
+
+def main():
+
+	usage = f'\n\n\tusage: {sys.argv[0]} "acc_list.txt"\n\n'
+
+	if len(sys.argv) < 2:
+		sys.stderr.write(usage)
+		sys.exit(1)
+
+	acc_file = sys.argv[1]
+
+	entrez_get_prots_from_acc(acc_file)
+
+if __name__ == '__main__':
+	main()
