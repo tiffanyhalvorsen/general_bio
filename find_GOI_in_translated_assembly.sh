5603df0386802e73d26db9206e5c74ca3c59b570
tiffanyhalvorsen <tiffany.marie.halvorsen@gmail.com>
2024-02-27 16:31:50 -0800

    diamond script works -- can search for a series of protein homologues (in ./queries) in any genome assemblies located in folder (as ./GCF_########.#/assembly.fna) after making diamond database file from assembly

--- find_GOI_in_translated_assembly.sh
+++ find_GOI_in_translated_assembly.sh
@@ -11,19 +11,58 @@
 sensitivity=$1
 output_location=$2
 
+# Search for all subdirectories in current directory
 
 find . -mindepth 1 -maxdepth 1 -type d | while IFS= read -r dir; do
 	
-	for db_file in $(find $dir -maxdepth 1 -type f -name "*.dmnd"); do
+	# Loop through each genome diamond database file
 
+	for db_file in $(find $dir -maxdepth 1 -type f -name "GC*.dmnd"); do
+
+		# Also loop through each query of interest in ./queries subfolder
+
+		# Blastp each .faa query file against each .dmnd genome database file
+		
 		for query in $(find ./queries -type f -name "*.faa"); do
 			queryname=$(basename $query .faa)
 			dbname=$(basename $db_file .dmnd)
-			if [[ $output_location == --current-dir ]]; then					
-				diamond blastp --query $query --db $db_file $sensitivity --outfmt 6 --header --out ${queryname}_blastp_results_${dbname}.out
-			else
-				diamond blastp --query $query --db $db_file $sensitivity --outfmt 6 --header --out $dir/$(basename -- $query)_blastp_results_$(basename -- $db_file).out
-			fi
-		done		
-	done
-done
+
+			# Check that diamond has not alreadyu been run for the queries in ./queries subfolder
+
+            if [ ! -s ${dir}/${queryname}_blastp_results_${dbname}.out ]; then 
+				echo -e "${queryname}_blastp_results_${dbname}.out -- blasting off now \n\n"
+
+				# User can specify that output gets placed in the current working directory
+
+				if [[ $output_location == --current-dir ]]; then					
+					diamond blastp --query $query --db $db_file $sensitiviity --outfmt 6 --header --out ${queryname}_blastp_results_${dbname}.out 2> diamond.log
+
+				# If current directory is not specified, output is placed in subdirectories with genome assembly folders
+
+				else
+					diamond blastp --query $query --db $db_file $sensitivity --outfmt 6 --header --out ${dir}/${queryname}_blastp_results_${dbname}.out 2> diamond.log
+				fi
+				
+				assembly=$(basename $dir)
+
+			# Add "Assembly_ID" and "Query_name"to the end of the Fields line and the
+			# corresponding variables to the end of each blastp hit in which it was found.
+
+			# Note: sed delimiter can change. Using comma here to avoid pattern matching 
+			# conflicts. Two sed commands, separated by ";". Must surround sed command 
+
+			# in double-quotes to expand the $assembly variable.
+
+				sed -i '' -e "/# Fields/ s,$,\tAssembly_ID,;/^# /!s,$,\t$assembly,;/# Fields/ s,$,\tQuery_name,;/^# /! s,$,\t$queryname," ${dir}/${queryname}_blastp_results_${dbname}.out
+			
+				# If database has already been queried, don't run again
+
+				else
+					echo -e  "${queryname} has already been queried against ${dbname}. Skipping.\n\n"
+		
+
+			fi		
+		
+			done		
+		done
+done	
